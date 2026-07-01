7e92b6904d326210f06dbfb3d6b6cf26eb485f55
PavuluriMounika <pavulurimounikaa@gmail.com>
2026-07-01 23:18:18 +0530

    Added search by item functionality in expense list using AJAX

--- src/main/webapp/js/expenseList.js
+++ src/main/webapp/js/expenseList.js
@@ -26,12 +26,29 @@ $(document).ready(function () {
             itemId: itemId
         },
         success: function(response) {
+            let rows = "";
+            $.each(response,function(i,expense){
+                console.log(rows);
+                rows += `
+                    <tr>
+                        <td>
+                            <a href="expenseView.action?expenseId=${expense.expenseId}">
+                                ${expense.expenseId}
+                            </a>
+                        </td>
+                        <td>${expense.itemId}</td>
+                        <td>${expense.amount}</td>
+                        <td>${expense.description}</td>
+                    </tr>`;
+                
+            });
 
             console.log(response);
-
+            
+            $("#expenseTableBody").html(rows);
         }
     });
 
-});
+    });
 
 });
